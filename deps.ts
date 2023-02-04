4e01b53fd954b6c963b2da8a294daf8d30ea2294
Alexiy Rybin <0xtapnisu@gmail.com>
2023-02-04 19:41:57 +0700

    Bump harmony version

--- deps.ts
+++ deps.ts
@@ -1,4 +1,5 @@
 export { serve } from "https://deno.land/std@0.143.0/http/server.ts";
 export { config } from "https://deno.land/x/dotenv@v3.2.0/mod.ts";
-export * from "https://deno.land/x/harmony@v2.6.0/mod.ts";
+export * from "https://deno.land/x/harmony@v2.8.0/mod.ts";
 export * as xeorarch from "https://deno.land/x/xeorarchx@v2.1.0/mod.ts";
+
